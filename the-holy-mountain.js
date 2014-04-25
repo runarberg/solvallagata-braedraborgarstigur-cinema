67c15a438510c2ee8e99b282bc109f0b2b6a6de7
Rúnar Berg Baugsson Sigríðarson <runarberg@zoho.com>
2014-04-25 12:54:14 +0000

    added history to holy mountain

--- the-holy-mountain.js
+++ the-holy-mountain.js
@@ -1,8 +1,8 @@
 $(window).scroll(function() {
-    var t_pos = $(window).scrollTop(),
-        b_pos = t_pos + $(window).height(),
-        $el = $(".info"),
-        el_top = $el.offset().top;
-    var l_pos = (t_pos - el_top) + ($el.width()/2);
-    $(".info").css( {left: l_pos});
+  var t_pos = $(window).scrollTop(),
+      b_pos = t_pos + $(window).height(),
+      $el = $(".info"),
+      el_top = $el.offset().top;
+  var l_pos = (t_pos - el_top) + ($el.width()/2);
+  $(".info").css( {left: l_pos});
 });
